61cd815132183dda6454e029a24f0a3ad9b4bc9c
Sajit Maharjan <sajitmhr13@gmail.com>
2025-01-29 19:01:40 +0545

    feat: error handling

--- src/controllers/Usercontroller.ts
+++ src/controllers/Usercontroller.ts
@@ -1,12 +1,14 @@
 export class UserController{
 
 
-     static login(req,res){
+     static login(req,res,next){
        
-            const data =[{name: 'tech'}]
-            res.status(200).send(data)
-         }
-
+            // const data =[{name: 'tech'}]
+            // res.status(200).send(data)
+            // const error=new Error('user email or password does not match')
+            // next(error)
+         res.send(req.body)
+          }
          static test1(req,res,next){
        
 console.log("test") 
--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,7 @@
 import * as express from "express";
 import * as mongoose from "mongoose"
 import { getEnvironmanetVariables } from "./environments/environment";
-
+import * as bodyParser from "body-parser"
 import UserRoutes from "./routers/UserRoutes";
 
 
@@ -12,10 +12,13 @@ export class Server{
    constructor(){
       this.setConfig()
       this.setRoutes()
+      this.errorhandler() 
+      this.handleErros() //call this function when error occur while url hit
    }
 
    setConfig(){
       this.connectMongoDB()
+      this.configureBodyParser()
    }
 
    connectMongoDB(){
@@ -27,7 +30,34 @@ mongoose.connect(getEnvironmanetVariables().db_uri)
 })
    }
 
+   configureBodyParser(){
+      this.app.use(bodyParser.urlencoded({extended:true})) //we can parse any data like string, object, array ...anything
+
+   }
+
    setRoutes(){
       this.app.use('/api/user',UserRoutes)
     }
-   }
+
+    errorhandler(){
+      this.app.use((req,res)=>{
+         res.status(404).json({
+            message: "server not found",
+            status: 404
+         })
+      })
+    }
+
+    handleErros(){
+      this.app.use((error,req,res,next)=>
+         {
+         const errorStatus =req.errorStatus || 500 
+         res.status(errorStatus).json
+         ({
+            message: error.message || "something went wrong",
+            status: errorStatus
+
+       
+         })
+         })}
+      }
